02d4bbe5f3c06bd8a196aa0b73027723299c98ba
Gyeonghun Min <riverpower6@naver.com>
2025-10-22 11:25:45 +0900

    [SETTING] DDD 연습 프로젝트 생성

---
+++ src/main/java/org/example/dddpractice/DddPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.dddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPracticeApplication.class, args);
+    }
+
+}
